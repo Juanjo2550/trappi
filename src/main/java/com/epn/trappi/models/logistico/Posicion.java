f65a21af146328961a9faf9adf1ea4cefa374bce
DESKTOP-L1E29KJ\IntelHPi5 <christian.moran@epn.edu.ec>
2021-02-21 21:50:50 -0500

    a.1

---
+++ src/main/java/com/epn/trappi/models/logistico/Posicion.java
@@ -0,0 +1,29 @@
+package com.epn.trappi.models.logistico;
+
+public class Posicion {
+    private double latitud;
+    private double longitud;
+
+    public Posicion() {
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+    
+    public void actualizarPosicion(){
+        
+    }
+}
