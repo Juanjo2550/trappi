087fcd7bc303b9399f1a59512e780d80f29b18ff
DESKTOP-L1E29KJ\IntelHPi5 <christian.moran@epn.edu.ec>
2021-02-21 23:00:20 -0500

    Conductor y ListaConductores

--- src/main/java/com/epn/trappi/models/logistico/Conductor.java
+++ src/main/java/com/epn/trappi/models/logistico/Conductor.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.epn.trappi.models.logistico;
 
-/**
- *
- * @author Erick
- */
 public class Conductor {
-    
+    private String nombre;
+    private boolean estado;
+
+    public Conductor(String nombre, boolean estado) {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
---
+++ src/main/java/com/epn/trappi/models/logistico/ListaConductores.java
@@ -0,0 +1,11 @@
+package com.epn.trappi.models.logistico;
+
+import java.util.ArrayList;
+
+public class ListaConductores {
+    private ArrayList<Conductor> listaConductores;
+
+    public ListaConductores() {
+        this.listaConductores = new ArrayList<Conductor>();
+    }
+}
