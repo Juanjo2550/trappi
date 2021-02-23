22301e1773fd5fdada4fd535075e2992354947cd
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-02-22 21:09:57 -0500

    SQLite implementation

---
+++ src/main/java/com/epn/trappi/db/rrhh/Connect.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.epn.trappi.db.rrhh;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Juan Jaramillo <juanjodev02 at juan.jaramillo02@epn.edu.ec>
+ */
+public class Connect {
+    public static Connection connect() {
+            Connection conn = null;
+            try {
+                if(conn == null) {
+                    String projectPath = System.getProperty("user.dir");
+                    // db parameters
+                    String url = "jdbc:sqlite:" + projectPath + "example.db";
+                    // create a connection to the database
+                    conn = DriverManager.getConnection(url);
+
+                    System.out.println("Connection to SQLite has been established.");
+                }
+                return conn;
+                
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+    }
+}
