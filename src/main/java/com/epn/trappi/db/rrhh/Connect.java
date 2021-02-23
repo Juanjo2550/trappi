c739977ff8cca9c1460c52ddb30db2388d8611c2
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-02-23 11:17:53 -0500

    SQLite db connection update

--- src/main/java/com/epn/trappi/db/rrhh/Connect.java
+++ src/main/java/com/epn/trappi/db/rrhh/Connect.java
@@ -15,13 +15,13 @@ import java.sql.SQLException;
  * @author Juan Jaramillo <juanjodev02 at juan.jaramillo02@epn.edu.ec>
  */
 public class Connect {
-    public static Connection connect() {
+    public static Connection connect(String databaseFileName) {
             Connection conn = null;
             try {
                 if(conn == null) {
                     String projectPath = System.getProperty("user.dir");
                     // db parameters
-                    String url = "jdbc:sqlite:" + projectPath + "/src/main/java/com/epn/trappi/db/rrhh/javier.db";
+                    String url = "jdbc:sqlite:" + projectPath + "/src/main/java/com/epn/trappi/db/rrhh/" + databaseFileName;
                     // create a connection to the database
                     conn = DriverManager.getConnection(url);
                     System.out.println("Connection to SQLite has been established.");
