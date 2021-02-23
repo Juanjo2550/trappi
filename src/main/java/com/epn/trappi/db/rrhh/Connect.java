bd5387c87e4199fe760bb85aef5276bb9c029e22
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-02-22 21:10:53 -0500

    SQLite connection fix

--- src/main/java/com/epn/trappi/db/rrhh/Connect.java
+++ src/main/java/com/epn/trappi/db/rrhh/Connect.java
@@ -21,24 +21,14 @@ public class Connect {
                 if(conn == null) {
                     String projectPath = System.getProperty("user.dir");
                     // db parameters
-                    String url = "jdbc:sqlite:" + projectPath + "example.db";
+                    String url = "jdbc:sqlite:" + projectPath + "juanjo.db";
                     // create a connection to the database
                     conn = DriverManager.getConnection(url);
-
                     System.out.println("Connection to SQLite has been established.");
                 }
-                return conn;
-                
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
-            } finally {
-                try {
-                    if (conn != null) {
-                        conn.close();
-                    }
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
             }
+            return conn;
     }
 }
