3b5916dc086cf36b91143e08c6ee12c770e63cae
Davidaguirre99 <Davidaguirre99@users.noreply.github.com>
2021-02-23 21:26:15 -0500

    Se ajusta el módulo a cambios en el diagrama de clases
    Se cambio la relación entre Solicitud de Pago con Pago y clase Pago con presupuesto.

--- src/main/java/com/epn/trappi/models/financiero/Pago.java
+++ src/main/java/com/epn/trappi/models/financiero/Pago.java
@@ -8,28 +8,43 @@ import java.util.TimeZone;
 public class  Pago {
     String fechadePago;
     LibroDiario libroDiario;
-    String cuentaPagar;
+    String nroCuenta;
     double monto;
+    Presupuesto presupuesto;
 
-    public Pago(String cuentaPagar, double monto, LibroDiario libroDiario) {
-        this.libroDiario=libroDiario;
-        this.cuentaPagar = cuentaPagar;
-        this.monto = monto;
-    }
-    
-    public void registrarPago(Pago pago){
-        //Se obtiene la fecha del momento del pago
+    public Pago(LibroDiario libroDiario, String nroCuenta, double monto) {
+        //Se calcula la fecha del pago
         Calendar fechaPagoObjeto = Calendar.getInstance(); 
         String dia = String.valueOf(fechaPagoObjeto.get(Calendar.DATE));
         String mes = String.valueOf(fechaPagoObjeto.get(Calendar.MONTH)+1);//se le suma uno debido a que enero es igual a cero
         String anio = String.valueOf(fechaPagoObjeto.get(Calendar.YEAR));
-        fechadePago = dia+'/'+mes+'/'+anio;
+        this.fechadePago = dia+'/'+mes+'/'+anio;
+        this.libroDiario = libroDiario;
+        this.nroCuenta = nroCuenta;
+        this.monto = monto;
+    }
+
+
+    
+    public void registrarPago(Pago pago){
+
         libroDiario.pagosRegistrados.add(pago);
     }
-    
+    public String realizarPago(Pago pago){
+        this.presupuesto=new Presupuesto(this.libroDiario);
+        String estadoSolicitud="Pendiente";
+        if(pago.monto<this.presupuesto.calcularPresupuesto()){
+            System.out.println("El presupuesto de TRAPPI es: "+this.presupuesto.calcularPresupuesto());
+            estadoSolicitud="Realizado";
+            this.registrarPago(pago);
+            System.out.println("El nuevo presupuesto: "+this.presupuesto.calcularPresupuesto());
+        }
+        System.out.println("Funcion Realizar Pago returns : "+estadoSolicitud);
+        return estadoSolicitud; 
+    }
     @Override
     public String toString() {
-        return "Pago: " + "fechadePago=" + fechadePago + ", cuentaPagar=" + cuentaPagar + ", monto=" + monto + '\n';
+        return "Pago: " + "fechadePago=" + fechadePago + ", cuentaPagar=" + nroCuenta + ", monto=" + monto + '\n';
     }
     public void realizarPago(String numeroCuenta, double monto){
         
