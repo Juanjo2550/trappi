7dd3a8f23db79dba9651102b446206aa269a6ae8
alexandersw98 <rommel.valdiviezo@epn.edu.ec>
2021-02-25 10:10:35 -0500

    Modificacion de clases - flujo entrega
    Se actualizo el contenido de los metodos en las clases Entrega y Control para que se tenga una idea de como sera el flujo de entrega de un paquete.

---
+++ src/main/java/com/epn/trappi/models/logistico/ControlDisponibilidad.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epn.trappi.models.logistico;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Erick
+ */
+public class ControlDisponibilidad {
+    private ArrayList<Vehiculo> vehiculos;
+    private ArrayList<Conductor> conductores;
+
+    public ControlDisponibilidad(ArrayList<Vehiculo> vehiculos, ArrayList<Conductor> conductores) {
+        this.vehiculos = vehiculos;
+        this.conductores = conductores;
+    }
+    
+    public Vehiculo actualizarEstado(Vehiculo vehiculo,Conductor conductor){
+        int matricula = vehiculo.getMatricula();
+        String identificador = conductor.getNombre(); //Debemos usar una cedula
+        /*
+        Se utiliza la matricula y el identificador para cambiar el estado de disponibilidad
+        en la base de datos que contenga la lista de conductores y vehiculos.
+        */
+        //Se cambia el estado de la instancia del vehiculo
+        vehiculo.actualizarEstado(new Inhabilitado(vehiculo));
+        return vehiculo;
+    }
+    
+    public void asignar(Posicion destino){
+        /*
+        Se asume que aquí se realiza el proceso de selección de un conductor y un vehiculo
+        esto da como resultado una instancia de vehiculo y conductor.
+        */
+        Vehiculo vehiculo=null;
+        Conductor conductor=null;
+        vehiculo = actualizarEstado(vehiculo,conductor);
+        /*
+        Luego de actualizar los estados de disponibildad, debemos crear un registro en la tabla
+        de entregas activas para poder ver las rutas que sigue cada vehiculo con una entrega en proceso.
+        */
+        Entrega entrega = null;
+        entrega.actualizarEstado(vehiculo,conductor,destino);
+        /*
+        Después de haber añadido este registro, vamos a llamar a un método que correra en segundo plano
+        simulando asi el tiempo que tarde en llegar el vehiculo a su destino. 
+        */
+        simularMovimiento(60);
+        /*
+        Finalmente, dado que ya se simuló el tiempo que tardó el conductor en llegar a su destino,
+        se asume que el producto se entrego exitosamente y se procede a registrar esta entrega
+        en la base de datos. Y también se retira el registro de entrega activa ya que 
+        ahora esta finalizada. Para esto usamos el método registrarEntrega() de la clase Entrega.
+        */
+        entrega.RegistrarEntrega(vehiculo, conductor);
+        
+        
+    }
+    public void simularMovimiento(int tiempo){
+        /*
+        Este metodo sirve para detener un momento el proceso de entrega con el objetivo de simular la entrega
+        fisica del producto.
+        */
+    }
+    
+}
--- src/main/java/com/epn/trappi/models/logistico/Entrega.java
+++ src/main/java/com/epn/trappi/models/logistico/Entrega.java
@@ -25,21 +25,38 @@ public class Entrega {
         this.destino = destino;
     }
     
-    public void actualizarEstado(){
-        
+    public void actualizarEstado(Vehiculo vehiculo,Conductor conductor,Posicion destino){
+        /*
+        utiliza los datos de parametros para crear un registro de entrega activa tal y como ya lo hemos
+        hecho en el panel de rutas, con la diferencia de que ahora se añade a una tabla en la base de datos
+        que contiene las entregas activas.
+        */
     }
     
-    public void confirmarEntrega(){
-        
+    public void confirmarEntrega(String datos){
+        //Se deserializa el String para obtener la direccion de destino y el nombre del cliente.
+        deserializar(datos);
+        //Se llama al método asignar del control de disponibilidad
+        ControlDisponibilidad control = null;
+        control.asignar(this.destino);
     }
     
-    public void RegistrarEntrega(){
-        
+    public void RegistrarEntrega(Vehiculo vehiculo,Conductor conductor){
+        /*
+        En este bloque se asume que se utiliza los datos recibidos y junto a los atributos de esta clase
+        como lo es la direccion de destino. Para crear un registro en la base de datos con la entrega
+        que se realizó.
+        */
+        /*
+        En este bloque se elimina el registro de entrega activa de la base de datos.
+        */
+        /*
+        En este bloque se cambia el estado del conductor y el vehiculo a disponible otra vez.
+        */
     }
     
     
-    public void determinarDestino(String direccion){
-        //colocar la posicion de string en formato de posicion
+    public void deserializar(String datos){
         
     }
     
