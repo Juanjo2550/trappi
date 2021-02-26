4c9d07341157a19bede5eae6cbacbee87d274098
Bryan Flores <bryan30s@hotmail.com>
2021-02-26 09:00:01 -0500

    Quitar error en clase de modelos ecommerce

--- src/main/java/com/epn/trappi/models/ecommerce/Ecommerce.java
+++ src/main/java/com/epn/trappi/models/ecommerce/Ecommerce.java
@@ -1,2 +1,8 @@
-Primera y segunda iteracion:
-https://lucid.app/lucidchart/invitations/accept/d70ef17b-80e1-44e6-b2d4-30c9c53774d2
+package com.epn.trappi.models.ecommerce;
+
+public class Ecommerce {
+    /*Primera y segunda iteracion:
+https://lucid.app/lucidchart/invitations/accept/d70ef17b-80e1-44e6-b2d4-30c9c53774d2*/
+}
+
+
