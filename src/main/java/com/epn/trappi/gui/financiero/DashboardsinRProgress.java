8ac4ccd3a6594b758ce074a26801cef958164623
Davidaguirre99 <Davidaguirre99@users.noreply.github.com>
2021-02-26 22:11:26 -0500

    Agrego tabla con panelIngresos
    Se agrego el panelIngresos en el panelDespliegue y se agrego una tabla

--- src/main/java/com/epn/trappi/gui/financiero/DashboardsinRProgress.java
+++ src/main/java/com/epn/trappi/gui/financiero/DashboardsinRProgress.java
@@ -20,13 +20,16 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
         btndashboardgeneral = new javax.swing.JPanel();
         lableGeneralDashboard = new javax.swing.JLabel();
         btndashboardingresos = new javax.swing.JPanel();
-        jLabel6 = new javax.swing.JLabel();
         btnDashboardSalir = new javax.swing.JPanel();
         labelDashboardSalir = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         panelHorizontalCabecera = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         panelDespliegue = new javax.swing.JPanel();
+        panelIngresos = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,7 +93,7 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
         lableGeneralDashboard.setForeground(new java.awt.Color(255, 255, 255));
         lableGeneralDashboard.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lableGeneralDashboard.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/epn/trappi/gui/financiero/imagenesFinanciero/icons8_increase_32px.png"))); // NOI18N
-        lableGeneralDashboard.setText("General");
+        lableGeneralDashboard.setText("Dashboard");
 
         javax.swing.GroupLayout btndashboardgeneralLayout = new javax.swing.GroupLayout(btndashboardgeneral);
         btndashboardgeneral.setLayout(btndashboardgeneralLayout);
@@ -102,10 +105,12 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
         );
         btndashboardgeneralLayout.setVerticalGroup(
             btndashboardgeneralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lableGeneralDashboard, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(btndashboardgeneralLayout.createSequentialGroup()
+                .addComponent(lableGeneralDashboard)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        panelVerticalIzquierdo.add(btndashboardgeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 180, 30));
+        panelVerticalIzquierdo.add(btndashboardgeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 190, 180, 30));
 
         btndashboardingresos.setOpaque(false);
         btndashboardingresos.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -117,26 +122,18 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
             }
         });
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI Semilight", 1, 12)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/epn/trappi/gui/financiero/imagenesFinanciero/icons8_money_pound_32px.png"))); // NOI18N
-        jLabel6.setText("Ingresos");
-
         javax.swing.GroupLayout btndashboardingresosLayout = new javax.swing.GroupLayout(btndashboardingresos);
         btndashboardingresos.setLayout(btndashboardingresosLayout);
         btndashboardingresosLayout.setHorizontalGroup(
             btndashboardingresosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(btndashboardingresosLayout.createSequentialGroup()
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addGap(0, 180, Short.MAX_VALUE)
         );
         btndashboardingresosLayout.setVerticalGroup(
             btndashboardingresosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 30, Short.MAX_VALUE)
         );
 
-        panelVerticalIzquierdo.add(btndashboardingresos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 190, 180, 30));
+        panelVerticalIzquierdo.add(btndashboardingresos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 180, 30));
 
         btnDashboardSalir.setOpaque(false);
         btnDashboardSalir.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -171,6 +168,13 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
 
         panelVerticalIzquierdo.add(btnDashboardSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 570, 180, 30));
 
+        jLabel6.setFont(new java.awt.Font("Segoe UI Semilight", 1, 12)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/epn/trappi/gui/financiero/imagenesFinanciero/icons8_money_pound_32px.png"))); // NOI18N
+        jLabel6.setText("Ingresos");
+        panelVerticalIzquierdo.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 50, 180, -1));
+
         jPanel1.add(panelVerticalIzquierdo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 180, 690));
 
         panelHorizontalCabecera.setBackground(new java.awt.Color(51, 51, 51));
@@ -191,6 +195,26 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
         jPanel1.add(panelHorizontalCabecera, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1370, 90));
 
         panelDespliegue.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        panelIngresos.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        panelIngresos.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 230, 890, 360));
+
+        panelDespliegue.add(panelIngresos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1190, 690));
+
         jPanel1.add(panelDespliegue, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 90, 1190, 690));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -273,10 +297,13 @@ public class DashboardsinRProgress extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JLabel labelDashboardSalir;
     private javax.swing.JLabel lableGeneralDashboard;
     private javax.swing.JPanel panelDespliegue;
     private javax.swing.JPanel panelHorizontalCabecera;
+    private javax.swing.JPanel panelIngresos;
     private javax.swing.JPanel panelVerticalIzquierdo;
     // End of variables declaration//GEN-END:variables
 }
