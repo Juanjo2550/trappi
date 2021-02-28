79f5eb1bc94077053b5bb4e7347566bb3e8600fc
Diego <diegomarquez2008@outlook.com>
2021-02-28 15:18:13 -0500

    conexión RRHH con financiero

---
+++ src/main/java/com/epn/trappi/models/rrhh/diego/SolicitudDePago.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epn.trappi.models.rrhh.diego;
+
+import com.epn.trappi.models.rrhh.Fecha;
+
+/**
+ *
+ * @author diego
+ */
+public class SolicitudDePago {
+    Fecha fechaSolicitud;
+    String estado;
+    
+    public void autorizarSolicitud(){
+        
+    }
+    public void solicitarPago(){
+        
+    }
+}
