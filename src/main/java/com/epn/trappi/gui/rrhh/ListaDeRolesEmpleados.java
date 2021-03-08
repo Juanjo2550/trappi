67c613a7e0895a141d4ecf71bf9fe9056cdde555
Diego <diegomarquez2008@outlook.com>
2021-03-07 23:41:22 -0500

    cambio solicitud pago

--- src/main/java/com/epn/trappi/gui/rrhh/ListaDeRolesEmpleados.java
+++ src/main/java/com/epn/trappi/gui/rrhh/ListaDeRolesEmpleados.java
@@ -37,10 +37,12 @@ public class ListaDeRolesEmpleados extends javax.swing.JFrame {
             "Número",
             "Nombre",
             "Apellido",
+            "Numero de Cuenta",
             "Fecha",
             "Total",
             "Descuentos",
-            "Estado"
+            "Estado",
+            
             
 
         };
@@ -85,6 +87,7 @@ public class ListaDeRolesEmpleados extends javax.swing.JFrame {
             "Número",
             "Nombre",
             "Apellido",
+            "Numero de Cuenta",
             "Fecha",
             "Total",
             "Descuentos",
@@ -98,6 +101,7 @@ public class ListaDeRolesEmpleados extends javax.swing.JFrame {
                 rol.getNumero(),
                 rol.getEmpleado().getNombres(),
                 rol.getEmpleado().getApellidos(),
+                rol.getEmpleado().getCuentaBancaria(),
                 rol.getFecha(),
                 rol.getTotal(),
                 rol.getDescuentos(),
@@ -312,7 +316,11 @@ public class ListaDeRolesEmpleados extends javax.swing.JFrame {
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         try {
+            
+            
             solicitudTable();
+            
+           
         } catch (Exception ex) {
             Logger.getLogger(ListaDeRolesEmpleados.class.getName()).log(Level.SEVERE, null, ex);
         }
