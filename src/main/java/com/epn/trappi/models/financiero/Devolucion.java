293183e58b9cb028d523e39a7d18342228de1411
Lesly <lesly.tipanluiza@epn.edu.ec>
2021-03-12 17:01:44 -0500

    Creación de la clase Devolución
    Se describieron los atributos y métodos de la clase Devolución.

---
+++ src/main/java/com/epn/trappi/models/financiero/Devolucion.java
@@ -0,0 +1,18 @@
+
+package com.epn.trappi.models.financiero;
+
+public class Devolucion {
+    Fecha fechaDeDevolucion;
+    //SolicitudDevolucion solicitudDevilucion;
+    String nroFactura;
+    double valorDevolucion;
+    
+    public double procesarDevolucion(){
+        double devolucion=0;
+        return devolucion;
+    }
+    private double calcularValorDevolucion(){
+        double devolucion=0;
+        return devolucion;
+    }
+}
