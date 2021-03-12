692be55e93a0efc1e22e48f6baf37ff05b41b769
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-03-11 20:32:26 -0500

    Data model interface

---
+++ src/main/java/com/epn/trappi/db/rrhh/ModelDb.java
@@ -0,0 +1,9 @@
+package com.epn.trappi.db.rrhh;
+
+import java.util.ArrayList;
+
+public interface ModelDb <T> {
+    public void agregar(T newObject);
+    public T buscarUno(String parametro);
+    public ArrayList<T> obtenerTodos();
+}
